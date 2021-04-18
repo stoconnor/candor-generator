5f3e58e2145f6c21f013ef0196609714fef89325
Stephen O'Connor <oconnor1986@gmail.com>
2021-04-18 10:39:06 +0100

    update ogContent URL

--- layouts/default.vue
+++ layouts/default.vue
@@ -22,7 +22,7 @@ export default {
         {
           hid: 'og:image',
           property: 'og:image',
-          content: this.BASE_URL + ogImage,
+          content: ogImage,
         },
       ],
     }
